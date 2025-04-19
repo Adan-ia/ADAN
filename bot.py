b397c12c5d832e3b6a280017aa21cad699650d6e
Adan-ia <migrupovivaldi@gmail.com>
2025-04-18 23:33:29 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,3 +15,16 @@ def responder(m):
 if __name__ == "__main__":
     print("⚡ Bot iniciado")
     bot.polling()
+
+
+from flask import Flask
+   app = Flask(__name__)
+
+   @app.route('/')
+   def home():
+       return "Bot activo (ignorar este mensaje)", 200
+
+   if __name__ == "__main__":
+       import threading
+       threading.Thread(target=bot.polling).start()
+       app.run(host='0.0.0.0', port=10000)
