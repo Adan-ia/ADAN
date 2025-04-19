850babb2a6c16d3e3e7e7945d60932c8e1c5ac07
Adan-ia <migrupovivaldi@gmail.com>
2025-04-18 23:46:40 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
 
 
 from flask import Flask
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   @app.route('/')
-   def home():
-       return "Bot activo (ignorar este mensaje)", 200
+@app.route('/')
+def home():
+    return "Bot activo (ignorar este mensaje)", 200
 
-   if __name__ == "__main__":
-       import threading
-       threading.Thread(target=bot.polling).start()
-       app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+import threading
+threading.Thread(target=bot.polling).start()
+app.run(host='0.0.0.0', port=10000)
